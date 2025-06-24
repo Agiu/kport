06bdc59e3f6267705f328f29dc02acb33893091f
kaelubagu <caguiar@trinity.edu>
2025-06-23 19:24:23 -0500

    added file

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+safelist: [
+  'dark:via-red-600',
+  'dark:to-red-600',
+  'dark:from-red-600',
+  'dark:via-blue-600',
+  'dark:to-blue-600',
+  'dark:from-blue-600',
+  'dark:via-zinc-950',
+  'dark:to-zinc-950',
+  'dark:from-zinc-950',
+  'dark:bg-red-950',
+  'dark:via-green-500',
+  'dark:to-green-500',
+  'dark:from-green-500',
+  'dark:bg-zinc-900'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
